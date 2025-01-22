7a38a48493731c67f841ef03a5d7805dee79316e
Elar Campanano <elarcampanano@Elars-MacBook-Air.local>
2025-01-22 10:52:47 +0800

    update voucher implem

--- delivery/src/main/java/com/gcash/exam/delivery/model/response/VoucherResponse.java
+++ delivery/src/main/java/com/gcash/exam/delivery/model/response/VoucherResponse.java
@@ -1,4 +1,22 @@
 package com.gcash.exam.delivery.model.response;
 
-public record VoucherResponse(String code, float discount, String expiry) {
+import java.util.List;
+import java.util.Optional;
+
+public record VoucherResponse(String voucherCode, double discount, String expiry) {
+    public static List<VoucherResponse> getDefaultVouchers() {
+        // Creating an immutable list with default voucher values
+        return List.of(
+                new VoucherResponse("MYNT", 15, "2025-12-30"),
+                new VoucherResponse("GFI", 10, "2025-12-30"),
+                new VoucherResponse("skdlks", 5, "2025-12-30"),
+                new VoucherResponse("expired", 10, "2024-12-30")
+        );
+    }
+
+    public static Optional<VoucherResponse> getValidVoucherDiscount(String inputVoucherCode) {
+        return getDefaultVouchers().stream()
+                .filter(voucher -> voucher.voucherCode().equals(inputVoucherCode)) // Match voucher code
+                .findFirst(); // Return the first match (if any)
+    }
 }
--- delivery/src/main/java/com/gcash/exam/delivery/service/VoucherServiceImpl.java
+++ delivery/src/main/java/com/gcash/exam/delivery/service/VoucherServiceImpl.java
@@ -5,9 +5,11 @@ import com.gcash.exam.delivery.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
+
+import static com.gcash.exam.delivery.model.response.VoucherResponse.getValidVoucherDiscount;
 
 @Service
 public class VoucherServiceImpl implements VoucherService {
@@ -15,7 +17,7 @@ public class VoucherServiceImpl implements VoucherService {
     private static final Logger log = LoggerFactory.getLogger(VoucherServiceImpl.class);
 
     // External MYNT API endpoint for fetching voucher data
-    private static final String VOUCHER_API_URL = "https://mynt-exam.mocklab.io/voucher/{voucherCode}";
+    //private static final String VOUCHER_API_URL = "https://mynt-exam.mocklab.io/voucher/{voucherCode}";
 
     // External MYNT API endpoint for fetching voucher data
     public double getVoucherDiscount(double totalCostBeforeDisc, String voucherCode) {
@@ -23,23 +25,25 @@ public class VoucherServiceImpl implements VoucherService {
         RestTemplate restTemplate = new RestTemplate();
 
         // Build the URI for the external API request
-        String url = UriComponentsBuilder.fromHttpUrl(VOUCHER_API_URL)
-                .queryParam("key", "apikey")
-                .toUriString();
+//        String url = UriComponentsBuilder.fromHttpUrl(VOUCHER_API_URL)
+//                .queryParam("key", "apikey")
+//                .toUriString();
 
         log.info("Calling outbound API...");
 
         // Send GET request to the external voucher API
-        VoucherResponse response = restTemplate.getForObject(url, VoucherResponse.class, voucherCode);
+//        VoucherResponse response = restTemplate.getForObject(url, VoucherResponse.class, voucherCode);
+
+        Optional<VoucherResponse> response = getValidVoucherDiscount(voucherCode);
 
         log.info("Success response from outbound API...");
 
-        // Check if the voucher was found and not expired then return the discount rate
-        if (!ObjectUtils.isEmpty(response) && !DateUtil.isExpired(response.expiry())) {
+        // Check if the voucher is present and not expired then return the discount rate
+        if (response.isPresent() && !DateUtil.isExpired(response.get().expiry())) {
             log.info("Voucher found and not expired...Applying discount...");
-            return applyDiscount(totalCostBeforeDisc, response.discount());
+            return applyDiscount(totalCostBeforeDisc, response.get().discount());
         } else {
-            log.info("Voucher expired");
+            log.info("Invalid voucher or has expired");
             return totalCostBeforeDisc;
         }
     }
